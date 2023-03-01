776c10669963f25ee7dc1ac12240b09d4efc9f67
KseniaStati <122460113+KseniaStati@users.noreply.github.com>
2023-03-01 21:15:47 +0400

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,7 @@
+
+valid_email = "tommycyberus@gmail.com"
+valid_password = "1234567890"
+empty_email = ""
+empty_password = ""
+empty_auth_key = {"key": ""}
+incorrect_auth_key = {"key": "000000000000000000000000000000000000000000000"}
