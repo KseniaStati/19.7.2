a900ba5fcc636d55073719d4ad82d8735abbb08f
KseniaStati <122460113+KseniaStati@users.noreply.github.com>
2023-03-01 21:18:41 +0400

    Add files via upload

---
+++ tests/test_pet_friends.py
@@ -0,0 +1,138 @@
+from api import PetFriends
+from settings import valid_email, valid_password, empty_password, empty_email, empty_auth_key, incorrect_auth_key
+import os
+
+pf = PetFriends()
+
+
+def test_get_api_key_for_valid_user(email=valid_email, password=valid_password):
+    """ Проверяем что запрос api ключа возвращает статус 200 и в тезультате содержится слово key"""
+
+    # Отправляем запрос и сохраняем полученный ответ с кодом статуса в status, а текст ответа в result
+    status, result = pf.get_api_key(email, password)
+
+    # Сверяем полученные данные с нашими ожиданиями
+    assert status == 200
+    assert 'key' in result
+
+
+def test_get_all_pets_with_valid_key(filter=''):
+    """ Проверяем что запрос всех питомцев возвращает не пустой список.
+    Для этого сначала получаем api ключ и сохраняем в переменную auth_key. Далее используя этого ключ
+    запрашиваем список всех питомцев и проверяем что список не пустой.
+    Доступное значение параметра filter - 'my_pets' либо '' """
+
+    _, auth_key = pf.get_api_key(valid_email, valid_password)
+    status, result = pf.get_list_of_pets(auth_key, filter)
+
+    assert status == 200
+    assert len(result['pets']) > 0
+
+
+def test_add_new_pet_with_valid_data(name='Барбоскин', animal_type='двортерьер', age='4'):
+    """Проверяем что можно добавить питомца с корректными данными"""
+    # Запрашиваем ключ api и сохраняем в переменую auth_key
+    _, auth_key = pf.get_api_key(valid_email, valid_password)
+    # Добавляем питомца
+    status, result = pf.add_new_pet(auth_key, name, animal_type, age)
+
+    # Сверяем полученный ответ с ожидаемым результатом
+    assert status == 200
+    assert result['name'] == name
+
+
+def test_successful_delete_self_pet():
+    """Проверяем возможность удаления питомца"""
+
+    # Получаем ключ auth_key и запрашиваем список своих питомцев
+    _, auth_key = pf.get_api_key(valid_email, valid_password)
+    _, my_pets = pf.get_list_of_pets(auth_key, "my_pets")
+
+    # Проверяем - если список своих питомцев пустой, то добавляем нового и опять запрашиваем список своих питомцев
+    if len(my_pets['pets']) == 0:
+        pf.add_new_pet(auth_key, "Суперкот", "кот", "3")
+        _, my_pets = pf.get_list_of_pets(auth_key, "my_pets")
+
+    # Берём id первого питомца из списка и отправляем запрос на удаление
+    pet_id = my_pets['pets'][0]['id']
+    status, _ = pf.delete_pet(auth_key, pet_id)
+
+    # Ещё раз запрашиваем список своих питомцев
+    _, my_pets = pf.get_list_of_pets(auth_key, "my_pets")
+
+    # Проверяем что статус ответа равен 200 и в списке питомцев нет id удалённого питомца
+    assert status == 200
+    assert pet_id not in my_pets.values()
+
+
+def test_successful_update_self_pet_info(name='Мурзик', animal_type='Котэ', age=5):
+    """Проверяем возможность обновления информации о питомце"""
+
+    # Получаем ключ auth_key и список своих питомцев
+    _, auth_key = pf.get_api_key(valid_email, valid_password)
+    _, my_pets = pf.get_list_of_pets(auth_key, "my_pets")
+
+    # Еслди список не пустой, то пробуем обновить его имя, тип и возраст
+    if len(my_pets['pets']) > 0:
+        status, result = pf.update_pet_info(auth_key, my_pets['pets'][0]['id'], name, animal_type, age)
+
+        # Проверяем что статус ответа = 200 и имя питомца соответствует заданному
+        assert status == 200
+        assert result['name'] == name
+    else:
+        # если спиок питомцев пустой, то выкидываем исключение с текстом об отсутствии своих питомцев
+        raise Exception("There is no my pets")
+
+def test_set_photo_pet(pet_photo="../image/453.jpg"):
+    """ Проверяем возможность добавления фото к информации о своем питомце """
+
+    # Запрашиваем ключ api и список своих питомцев
+    _, auth_key = pf.get_api_key(valid_email, valid_password)
+    _, my_pets = pf.get_list_of_pets(auth_key, "my_pets")
+
+    # Если список не пустой, то пробуем добавить фото
+    if len(my_pets["pets"]) > 0:
+        status, result = pf.set_photo_pet(auth_key, my_pets["pets"][0]["id"], pet_photo)
+
+        # Сверяем полученный ответ с ожидаемым результатом
+        assert status == 200
+        assert result["pet_photo"]
+    else:
+        # если список питомцев пустой, то выкидывает исключение с текстом об отсутствии своих питомцев
+        raise Exception("No pets")
+
+def test_get_api_key_for_password_user_empty(email=valid_email, password=empty_password):
+    """ Проверяем что запрос api ключа c валидным значением логина и пустым значением пароля возвращает статус 403
+    и в результате не содержится слово key """
+    # Отправляем запрос и сохраняем полученный ответ с кодом статуса в status, а текст ответа в result
+    status, result = pf.get_api_key(email, password)
+    # Сверяем полученный ответ с ожидаемым результатом
+    assert status == 403
+    assert "key" not in result
+
+def test_get_api_key_for_email_user_empty(email=empty_email, password=valid_password):
+    """ Проверяем что запрос api ключа c невалидным значением логина и  валидным значением пароля возвращает статус 403
+    и в результате не содержится слово key """
+    # Отправляем запрос и сохраняем полученный ответ с кодом статуса в status, а текст ответа в result
+    status, result = pf.get_api_key(email, password)
+    # Сверяем полученный ответ с ожидаемым результатом
+    assert status == 403
+    assert "key" not in result
+
+def test_get_api_key_for_email_and_password_user_empty(email=empty_email, password=empty_password):
+    """ Проверяем что запрос api ключа c невалидным значением логина и невалидным значением пароля возвращает статус 403
+    и в результате не содержится слово key """
+    # Отправляем запрос и сохраняем полученный ответ с кодом статуса в status, а текст ответа в result
+    status, result = pf.get_api_key(email, password)
+    # Сверяем полученный ответ с ожидаемым результатом
+    assert status == 403
+    assert "key" not in result
+
+def test_get_api_key_for_email_and_password_user_empty(email=empty_email, password=empty_password):
+    """ Проверяем что запрос api ключа c невалидным значением логина и невалидным значением пароля возвращает статус 403
+    и в результате не содержится слово key """
+    # Отправляем запрос и сохраняем полученный ответ с кодом статуса в status, а текст ответа в result
+    status, result = pf.get_api_key(email, password)
+    # Сверяем полученный ответ с ожидаемым результатом
+    assert status == 403
+    assert "key" not in result
